225af058396abbb68550d991154b86c5cce43848
Mariia Bazhina <kat.misato@gmail.com>
2020-08-16 16:04:45 +0300

    fix ActionMessage

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -83,7 +83,7 @@ class ArticleViewModel(private val articleId: String) :
         val msg = if (currentState.isLike) Notify.TextMessage("Mark is liked")
         else {
             Notify.ActionMessage(
-                    "Don't like it anymore", "No, still like it",
+                    "Don`t like it anymore", "No, still like it",
                     toggleLike
             )
         }
