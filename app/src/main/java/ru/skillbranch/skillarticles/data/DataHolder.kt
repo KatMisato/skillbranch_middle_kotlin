e2f462e6873c99f1c3564d2a6ade5bf42aaf092d
Mariia Bazhina <kat.misato@gmail.com>
2020-12-27 22:59:11 +0300

    replace local repository to network

--- app/src/main/java/ru/skillbranch/skillarticles/data/DataHolder.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/DataHolder.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import ru.skillbranch.skillarticles.data.EntityGenerator.generateArticleItems
 import ru.skillbranch.skillarticles.data.EntityGenerator.generateComments
 import ru.skillbranch.skillarticles.data.models.*
 import java.util.*
@@ -14,21 +15,21 @@ object LocalDataHolder {
     private val articleInfo = MutableLiveData<ArticlePersonalInfo?>(null)
     private val settings = MutableLiveData(AppSettings())
     private val isAuth = MutableLiveData(false)
-    val localArticleItems: MutableList<ArticleItemData> =  EntityGenerator.generateArticleItems(50) as MutableList<ArticleItemData>
+    val localArticleItems: MutableList<ArticleItemData> = mutableListOf()
     val localArticles: MutableMap<String, MutableLiveData<ArticleData>> = mutableMapOf()
 
     fun findArticle(articleId: String): LiveData<ArticleData?> {
         if (localArticles[articleId] == null) {
             Log.e("DataHolder", "findArticle $articleId: ");
             val article = localArticleItems.find { it.id == articleId }
-            localArticles[articleId] = MutableLiveData(EntityGenerator.generateArticle(article
-                    ?: EntityGenerator.createArticleItem(articleId)))
+            localArticles[articleId] = MutableLiveData(EntityGenerator.generateArticle(article ?: EntityGenerator.createArticleItem(articleId)))
         }
         return localArticles[articleId]!!
     }
 
     fun findArticlePersonalInfo(articleId: String): LiveData<ArticlePersonalInfo?> {
         GlobalScope.launch {
+            delay(500)
             articleInfo.postValue(ArticlePersonalInfo(isBookmark = true))
         }
         return articleInfo
@@ -51,15 +52,14 @@ object LocalDataHolder {
 
     fun incrementCommentsCount(articleId: String) {
         val old =
-                localArticles[articleId]?.value
-                        ?: error("Local article with id: $articleId not found")
+            localArticles[articleId]?.value ?: error("Local article with id: $articleId not found")
         localArticles[articleId]!!.postValue(old.copy(commentCount = old.commentCount.inc()))
     }
 }
 
 object NetworkDataHolder {
 
-    val networkArticleItems: List<ArticleItemData> = EntityGenerator.generateArticleItems(200)
+    val networkArticleItems: List<ArticleItemData> = generateArticleItems(200)
 
     val commentsData: Map<String, MutableList<CommentItemData>> by lazy {
         networkArticleItems.associate { article ->
@@ -70,6 +70,7 @@ object NetworkDataHolder {
     fun loadArticleContent(articleId: String): LiveData<String?> {
         val content = MutableLiveData<String?>(null)
         GlobalScope.launch {
+            delay(1500)
             val s = articlesContent[articleId.toInt() % 6]
             content.postValue(s)
         }
@@ -78,22 +79,27 @@ object NetworkDataHolder {
 
     fun sendMessage(articleId: String, text: String, answerToSlug: String?, user: User) {
         val mutableList =
-                commentsData[articleId] ?: error("Comments for article id : $articleId not found")
+            commentsData[articleId] ?: error("Comments for article id : $articleId not found")
         val index =
-                if (answerToSlug == null) 0 else mutableList.indexOfFirst { it.slug == answerToSlug }.inc()
+            if (answerToSlug == null) 0 else mutableList.indexOfFirst { it.slug == answerToSlug }.inc()
         val mess = mutableList.getOrNull(index.dec())
         val id = "${mutableList.size}"
         mutableList.add(
-                index,
-                CommentItemData(
-                        id,
-                        articleId,
-                        user,
-                        body = text,
-                        slug = "${answerToSlug ?: ""}$id/",
-                        answerTo = mess?.user?.name,
-                        date = Date()
-                )
+            index,
+            CommentItemData(
+                id,
+                articleId,
+                user,
+                body = text,
+                slug = "${answerToSlug ?: ""}$id/",
+                answerTo = mess?.user?.name,
+                date = Date()
+            )
         )
     }
 }
+
+
+
+
+
